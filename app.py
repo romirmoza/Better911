58710a0b7a9ef1fd414b6efd04a025b5a8b14fcb
Romir Moza <romirmoza@Romirs-MacBook-Air.local>
2019-10-10 15:07:35 -0400

    temp commit

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+import flask
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import render_template
+
+import folium
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-@app.route("/")
+@app.route('/')
 def index():
-        return "Welcome to Better911!"
+    map = folium.Map(location=[39.29, -76.61], zoom_start=11)
+    folium.GeoJson(
+            './static/Baltimore_City_Police_District_Boundary_Map.geojson',
+            name='geojson'
+            ).add_to(map)
+    map.save('./static/map.html')
+    return render_template('index.html')
+
+@app.route('/static/map.html')
+def show_map():
+    return flask.send_file('./static/map.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
